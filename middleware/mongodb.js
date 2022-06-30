641555b5f22c390e8cf56f4adf3b3d1b3d26a8f4
Shmuel <s600hho@gmail.com>
2022-06-30 09:53:34 +0300

    mongo

---
+++ middleware/mongodb.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const connectDB = handler => async (req, res) => {
+    if (mongoose.connections[0].readyState) {
+        // Use current db connection
+        return handler(req, res);
+    }
+    // Use new db connection
+    const { DB_USER, DB_PASS, DB_HOST, DB_NAME } = process.env;
+    console.log("DB_USER", DB_USER);
+    await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}`);
+    return handler(req, res);
+};
+
+export default connectDB;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const { DB_USER, DB_PASS, DB_HOST, DB_NAME } = process.env;
+
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    DB_USER, DB_PASS, DB_HOST, DB_NAME
+  }
 }
 
 module.exports = nextConfig
